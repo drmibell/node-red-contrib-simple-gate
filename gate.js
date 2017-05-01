2ecccddff65f2141fe677be459ed11244c343ea2
Michael I Bell <dr.m.i.bell@gmail.com>
2017-05-01 19:27:45 -0400

    Clean up javascript

--- gate.js
+++ gate.js
@@ -4,30 +4,12 @@ module.exports = function(RED) {
         var node = this;
         this.on('input', function(msg) {
 			var context = this.context();
-//			controlTopic = config.controlTopic;
 			this.controlTopic = config.controlTopic.toLowerCase();
 			this.openCmd = config.openCmd.toLowerCase();
 			this.closeCmd = config.closeCmd.toLowerCase();
 			this.toggleCmd = config.toggleCmd.toLowerCase();
 			this.defaultCmd = config.defaultCmd.toLowerCase();
 			this.defaultState = config.defaultState.toLowerCase();
-			
-/*			var controlTopic = this.controlTopic;
-			var defaultState = this.defaultState;
-			var openCmd = this.openCmd;
-			var closeCmd = this.closeCmd;
-			var toggleCmd = this.toggleCmd;
-			var defaultCmd = this.defaultCmd;
-*/			
-//			node.warn(toggleCmd);
-//			node.warn(defaultCmd);
-/*		var controlTopic = 'control';
-			var openCmd = 'open';
-			var closeCmd = 'close';
-			var toggleCmd = 'toggle';
-			var defaultCmd = 'default';
-			var defaultState = 'open';
-*/
 			var openStatus = {fill:"green",shape:"dot"};
 			var closedStatus = {fill:"red",shape:"dot"};
 			var state = context.get('state') || this.defaultState;
@@ -38,20 +20,16 @@ module.exports = function(RED) {
 			    status = closedStatus;
 			}
 			node.status(status);
-//			if (msg.topic == controlTopic) {
 			if (msg.topic.toLowerCase() == this.controlTopic) {
 				switch (msg.payload.toLowerCase()) {
-//					case 'open':
 					case this.openCmd:
 						state = 'open';
 						status = openStatus;
 						break;
-//					case 'close':
 					case this.closeCmd:
 						state = 'closed';
 						status = closedStatus;
 						break;
-//					case 'toggle':
 					case this.toggleCmd:
 						if (state == 'open') {
 							state = 'closed';
@@ -61,7 +39,6 @@ module.exports = function(RED) {
 							status = openStatus;
 						}
 						break;
-//					case 'default':
 					case this.defaultCmd:
 						state = this.defaultState;
 						if (state == 'open') {
@@ -83,7 +60,6 @@ module.exports = function(RED) {
 				} else {
 					node.send(null);
 			}
-//
 					});
 				}
 				RED.nodes.registerType("gate",GateNode);
