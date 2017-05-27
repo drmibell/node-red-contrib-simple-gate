0d3dfd3049d3051a36c3cef4434a2e3e9935ad4d
Michael I Bell <dr.m.i.bell@gmail.com>
2017-05-27 18:33:44 -0400

    display status on deploy

--- gate.js
+++ gate.js
@@ -17,7 +17,16 @@
 module.exports = function(RED) {
     function GateNode(config) {
         RED.nodes.createNode(this,config);
-        var node = this;
+        this.defaultState = config.defaultState.toLowerCase();
+        var openStatus = {fill:"green",shape:"dot"};
+        var closedStatus = {fill:"red",shape:"dot"};
+        var state = this.defaultState;
+        if (state === 'open') {
+        	this.status (openStatus);
+        } else {
+        	this.status (closedStatus);
+        }
+        this.warn ('starting up v0.0.2.4')
         this.on('input', function(msg) {
             var context = this.context();
             this.controlTopic = config.controlTopic.toLowerCase();
@@ -25,58 +34,52 @@ module.exports = function(RED) {
             this.closeCmd = config.closeCmd.toLowerCase();
             this.toggleCmd = config.toggleCmd.toLowerCase();
             this.defaultCmd = config.defaultCmd.toLowerCase();
-            this.defaultState = config.defaultState.toLowerCase();
-            var openStatus = {fill:"green",shape:"dot"};
-            var closedStatus = {fill:"red",shape:"dot"};
             var state = context.get('state') || this.defaultState;
-            var status;
             if (state === 'open') {
-                status = openStatus;
+                this.status (openStatus);
             } else {
-                status = closedStatus;
+                this.status (closedStatus);
             }
-            node.status(status);
+           // Change state
             if (msg.topic.toLowerCase() === this.controlTopic) {
                 switch (msg.payload.toLowerCase()) {
                     case this.openCmd:
                         state = 'open';
-                        status = openStatus;
                         break;
                     case this.closeCmd:
                         state = 'closed';
-                        status = closedStatus;
                         break;
                     case this.toggleCmd:
                         if (state === 'open') {
                             state = 'closed';
-                            status = closedStatus;
                         } else {
                             state = 'open';
-                            status = openStatus;
                         }
                         break;
                     case this.defaultCmd:
                         state = this.defaultState;
-                        if (state === 'open') {
-                            status = openStatus;
-                        } else {
-                            status = closedStatus;
-                        }
                         break;
                     default:
-                        node.error('Invalid command');
+                        this.error('Invalid command');
                         break;
                 }
+                // Save state
                 context.set('state',state);
-                node.status(status);
-                node.send(null);
-            }
-            else if (state === 'open') {
-                    node.send(msg);
+                // Show status                
+                if (state === 'open') {
+                    this.status (openStatus);
                 } else {
-                    node.send(null);
-            }
-                    });
+                    this.status (closedStatus);
                 }
-                RED.nodes.registerType("gate",GateNode);
+                this.send(null);
             }
+            // Transmit message
+            else if (state === 'open') {
+                    this.send(msg);
+                } else {
+                    this.send(null);
+            }
+        });
+    }
+    RED.nodes.registerType("gate",GateNode);
+}
